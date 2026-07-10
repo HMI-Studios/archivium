9031bf0afb7d24c386d2a81b5e909e96411a91fc
Johannes Niemelä <johannes@niemela.se>
2026-07-10 09:55:58 -0500

    table sort should support numbers

--- dist/static/scripts/table.js
+++ dist/static/scripts/table.js
@@ -5,9 +5,11 @@ function sort(tbody, f, asc=true) {
   rows.forEach(row => tbody.appendChild(row));
 }
 
-function sortByCol(tbody, col, asc=true) {
+function sortByCol(tbody, col, asc=true, numeric=false) {
   sort(tbody, (...rows) => {
-    const [a, b] = rows.map(row => row.children[col].dataset.sort);
+    const [a, b] = rows
+      .map(row => row.children[col].dataset.sort)
+      .map(value => numeric ? Number(value) : value);
     if (a > b) return 1;
     else if (b > a) return -1;
     else return 0;
@@ -20,7 +22,7 @@ window.addEventListener('load', () => {
     table.querySelectorAll('th').forEach((th, i) => {
       if (th.classList.contains('sortable')) {
         th.onclick = () => {
-          sortByCol(tbody, i, table.dataset.sortedCol != i+1);
+          sortByCol(tbody, i, table.dataset.sortedCol != i+1, th.classList.contains('sort-numeric'));
           table.dataset.sortedCol = table.dataset.sortedCol != i+1 ? i+1 : `-${i+1}`;
         }
       }
